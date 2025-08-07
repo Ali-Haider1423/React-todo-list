63c0a73c11b9f9f7c8768b5cab6f73c7449c7aec
Ali haider <158279950+Ali-Haider1423@users.noreply.github.com>
2025-08-08 00:09:46 +0500

    setup starting code for the project

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,22 @@ import './App.css'
 
 function App() {
 
-  return 
+  return <div className="container">
+    <div className="heading">
+      <h1>To-Do List</h1>
+    </div>
+    <div className="form">
+      <input type="text" />
+      <button>
+        <span>Add</span>
+      </button>
+    </div>
+    <div>
+      <ul>
+        <li>A Item</li>
+      </ul>
+    </div>
+  </div>
 }
 
 export default App
