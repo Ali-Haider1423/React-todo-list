60022c5ded9e8ff0aa2487e2475a890d13035362
Ali haider <158279950+Ali-Haider1423@users.noreply.github.com>
2025-08-10 15:45:48 +0500

    added some extra functionalities

--- src/components/INPUT.jsx
+++ src/components/INPUT.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+
+
 
 function INPUT(props) {
+
+    var [input, setinput] = useState("");
+    function handleSub() {
+        setinput("");
+    };
+    function handleChange(event) {
+        setinput(event.target.value);
+    };
+
     return (
         <div className="form">
-            <form onSubmit={props.handleSubmit}>
-                <input type="text" name='todoItem' />
-                <button type='Submit'><span>Add</span></button>
+            {/* to trigger different functions onSubmit we have to use a call back function and pass an event which will contain the information that onSubmit target  */}
+            <form onSubmit={(event) => {
+                props.handleSubmit(event);
+                handleSub(event);
+                event.preventDefault();
+            }}>
+                <input onChange={handleChange} type="text" name='todoItem' value={input} />
+                <button type='Submit' ><span>Add</span></button>
             </form>
-        </div>
+        </div >
     );
+
 };
 
 export default INPUT;
